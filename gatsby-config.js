8445b59c207f829c341718fddc927ab3f1db6993
stormcloud266 <allprt89@gmail.com>
2018-09-12 21:55:31 -0600

    added url data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    // 'gatsby-plugin-robots-txt',
+    'gatsby-plugin-robots-txt',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
